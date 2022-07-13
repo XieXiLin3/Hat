d35e965ddd6581c3b8915c33573f30d8835b8af8
XieXiLin3 <xiexilin158@qq.com>
2022-07-13 12:50:58 +0800

    Update Something

--- src/main/java/cn/xiexilin/top/hat/hat/commands/HatCommand.java
+++ src/main/java/cn/xiexilin/top/hat/hat/commands/HatCommand.java
@@ -1,6 +1,6 @@
 package cn.xiexilin.top.hat.hat.commands;
 
-import org.bukkit.Color;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,18 +11,20 @@ public class HatCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage(Color.RED + "你不是玩家,怎么设置呢 :(");
+            sender.sendMessage(ChatColor.RED + "你不是玩家,怎么设置呢 :(");
             return true;
         }
         Player p=(Player)sender;
         ItemStack item= p.getInventory().getItemInMainHand();
         if (item == null || item.getType().isAir()) {
-            sender.sendMessage(Color.RED + "你手上是空气哦 :(");
+            sender.sendMessage(ChatColor.RED + "你手上是空气哦 :(");
             return true;
         }
+        ItemStack helmetitem= p.getInventory().getHelmet();
         p.getInventory().setHelmet(item);
         p.getInventory().removeItem(item);
-        p.sendMessage(Color.GREEN + "已成功设置您的帽子为" + item.getItemMeta().getDisplayName());
+        p.getInventory().setItemInMainHand(helmetitem);
+        p.sendMessage(ChatColor.GREEN + "已成功设置您的帽子为" + item.getItemMeta().getDisplayName());
         return false;
     }
 }
